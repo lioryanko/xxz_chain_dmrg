120472f8a339393d8d42ee782f6c6798da46c434
Lior <lioryankovich92@gmail.com>
2020-12-05 12:22:41 +0200

    Changed the basis order of the superblock to an easier representation |al>|s1>|bl>|s2> instead of |al>|s1>|s2>|bl>. Changed gs.T to np.conjugate(gs.T) where approriate (at locations such as norm calculation).

--- dmrg_svd.py
+++ dmrg_svd.py
@@ -2,17 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 
-sp = np.array([[0, 1], [0, 0]])
 sz = np.array([[1, 0], [0, -1]])
-
-sz_sz = np.kron(sz, sz)
-sp_sp = np.kron(sp, sp)
-sm_sm = np.kron(sp.T, sp.T)
-
-
-def left_block_operator_to_right_block(operator):
-    U = np.flip(np.eye(len(operator)), 0)
-    return U @ operator @ U
+sp = np.array([[0, 1], [0, 0]])
+sm = sp.T
 
 
 class XXZChain(object):
@@ -21,41 +13,39 @@ class XXZChain(object):
         self.h = h
         self.Jz = Jz
         self.J = J
-        self.left_block_H = - h * sz
+        self.block_H = - h * sz
         self.last_site_sz = sz
         self.last_site_sp = sp
-        self.last_site_sm = sp.T
-        self.left_block_M = sz
+        self.last_site_sm = sm
+        self.block_M = sz
 
     def build_super_block_hamiltonian(self):
-        block_dimension = len(self.left_block_H)
+        block_dimension = len(self.block_H)
         d = len(sz)
 
         # Add the single block terms.
-        right_block_H = left_block_operator_to_right_block(self.left_block_H)
-        super_block_H = np.kron(self.left_block_H, np.eye(d**2 * block_dimension))
-        super_block_H += np.kron(np.eye(block_dimension * d**2), right_block_H)
+        super_block_H = np.kron(self.block_H, np.eye(d * block_dimension * d))
+        super_block_H += np.kron(np.eye(block_dimension * d), np.kron(self.block_H, np.eye(d)))
 
         # Add the single site terms.
         super_block_H += np.kron(np.kron(np.eye(block_dimension), -self.h * sz), np.eye(d * block_dimension))
-        super_block_H += np.kron(np.kron(np.eye(block_dimension * d), -self.h * sz), np.eye(block_dimension))
+        super_block_H += np.kron(np.eye(block_dimension * d * block_dimension), -self.h * sz)
 
         # Add the site-site interaction terms.
-        site_site_interaction = self.J / 2 * (sp_sp + sm_sm) + self.Jz * sz_sz
-        super_block_H += np.kron(np.kron(np.eye(block_dimension), site_site_interaction), np.eye(block_dimension))
+        site_sz = np.kron(np.eye(block_dimension), sz)
+        site_sp = np.kron(np.eye(block_dimension), sp)
+        site_sm = np.kron(np.eye(block_dimension), sm)
+        super_block_H += self.Jz * np.kron(site_sz, site_sz)
+        super_block_H += self.J / 2 * np.kron(site_sp, site_sp)
+        super_block_H += self.J / 2 * np.kron(site_sm, site_sm)
 
         # Add the block-site interaction terms.
-        left_block_edge_interaction = -self.Jz * np.kron(self.last_site_sz, sz)
-        left_block_edge_interaction += -self.J / 2 * np.kron(self.last_site_sp, sp)
-        left_block_edge_interaction += -self.J / 2 * np.kron(self.last_site_sm, sp.T)
+        block_site_interaction = self.Jz * np.kron(self.last_site_sz, sz)
+        block_site_interaction += self.J / 2 * np.kron(self.last_site_sp, sp)
+        block_site_interaction += self.J / 2 * np.kron(self.last_site_sm, sm)
 
-        U = np.flip(np.eye(block_dimension * d), 0)
-        right_block_edge_interaction = U @ left_block_edge_interaction @ U
-
-        super_block_H += np.kron(left_block_edge_interaction, np.eye(len(left_block_edge_interaction)))
-        super_block_H += np.kron(np.eye(len(right_block_edge_interaction)), right_block_edge_interaction)
-
-        super_block_H += np.kron(left_block_edge_interaction, np.eye(d * block_dimension))
+        super_block_H += np.kron(block_site_interaction, np.eye(len(block_site_interaction)))
+        super_block_H += np.kron(np.eye(len(block_site_interaction)), block_site_interaction)
 
         return super_block_H
 
@@ -65,50 +55,49 @@ class XXZChain(object):
         return eigvals[0], eigvects[:, 0]
 
     def get_super_block_ground_state_magnetization(self):
-        block_dimension = len(self.left_block_H)
+        block_dimension = len(self.block_H)
         d = len(sz)
 
-        right_block_M = left_block_operator_to_right_block(self.left_block_M)
-        super_block_M = np.kron(self.left_block_M, np.eye(d ** 2 * block_dimension))
-        super_block_M += np.kron(np.eye(block_dimension * d ** 2), right_block_M)
+        super_block_M = np.kron(self.block_M, np.eye(d ** 2 * block_dimension))
+        super_block_M += np.kron(np.eye(block_dimension * d), np.kron(self.block_M, np.eye(d)))
         super_block_M += np.kron(np.kron(np.eye(block_dimension), sz), np.eye(d * block_dimension))
-        super_block_M += np.kron(np.kron(np.eye(block_dimension * d), sz), np.eye(block_dimension))
+        super_block_M += np.kron(np.eye(block_dimension * d * block_dimension), sz)
 
         _, gs = self.get_super_block_ground_state()
-        return (gs.T @ super_block_M @ gs) / (gs.T @ gs)
+        return (np.conjugate(gs.T) @ super_block_M @ gs) / (np.conjugate(gs.T) @ gs)
 
     def get_compression_matrices(self):
         _, ground_state = self.get_super_block_ground_state()
         rho = np.einsum('i,j->ij', ground_state, np.conjugate(ground_state))
-        rho_reduced_dimension = len(self.left_block_H) * len(sz)
+        rho_reduced_dimension = len(self.block_H) * len(sz)
         rho_reduced = rho.reshape(rho_reduced_dimension, rho_reduced_dimension, rho_reduced_dimension,
                                   rho_reduced_dimension).trace(axis1=2, axis2=3)
         U, s, V = np.linalg.svd(rho_reduced)
         d = min(self.max_dimension, len(s))
         U_c = U[:d]
-        V_c = V[:,:d]
+        V_c = V[:, :d]
         return U_c, V_c
 
     def expand(self, iterations):
         for i in range(iterations):
-            block_dimension = len(self.left_block_H)
+            block_dimension = len(self.block_H)
             next_site_sz = np.kron(np.eye(block_dimension), sz)
             next_site_sp = np.kron(np.eye(block_dimension), sp)
-            next_site_sm = np.kron(np.eye(block_dimension), sp.T)
+            next_site_sm = np.kron(np.eye(block_dimension), sm)
 
             block_site_interaction = self.Jz * np.kron(self.last_site_sz, sz)
-            block_site_interaction += self.J / 2 * (np.kron(self.last_site_sp, sp) + np.kron(self.last_site_sm, sp.T))
+            block_site_interaction += self.J / 2 * (np.kron(self.last_site_sp, sp) + np.kron(self.last_site_sm, sm))
 
             U, V = self.get_compression_matrices()
 
-            self.left_block_H = np.kron(self.left_block_H, np.eye(len(sz)))
-            self.left_block_H += -self.h * next_site_sz + block_site_interaction
-            self.left_block_M = np.kron(self.left_block_M, np.eye(len(sz)))
-            self.left_block_M += next_site_sz
+            self.block_H = np.kron(self.block_H, np.eye(len(sz)))
+            self.block_H += -self.h * next_site_sz + block_site_interaction
+            self.block_M = np.kron(self.block_M, np.eye(len(sz)))
+            self.block_M += next_site_sz
 
             # Now the block has another site, and the operators operating on it are (possibly) compressed.
-            self.left_block_H = U @ self.left_block_H @ V
-            self.left_block_M = U @ self.left_block_M @ V
+            self.block_H = U @ self.block_H @ V
+            self.block_M = U @ self.block_M @ V
             self.last_site_sz = U @ next_site_sz @ V
             self.last_site_sp = U @ next_site_sp @ V
             self.last_site_sm = U @ next_site_sm @ V
